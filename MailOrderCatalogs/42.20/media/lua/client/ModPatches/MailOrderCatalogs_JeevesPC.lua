c861e791c881a5485c8dbf09d3d68b33e1310a65
Jusblazm <ryanlarkin0@gmail.com>
2026-07-29 22:53:21 -0700

    v3.0 - build 42.20 stable release

---
+++ MailOrderCatalogs/42.20/media/lua/client/ModPatches/MailOrderCatalogs_JeevesPC.lua
@@ -0,0 +1,32 @@
+-- -- MailOrderCatalogs_JeevesPC
+-- local MailOrderCatalogs_Utils = require("MailOrderCatalogs_Utils")
+
+-- --[[
+--     this patch allows Mail Order Catalogs to function on Jeeve's PCs
+-- ]]
+
+-- local function patchJeevesPCs()
+--     if not getActivatedMods():contains("JeevesPC") then
+--         print("[MailOrderCatalogs] General: Jeeve's PC is not installed")
+--         return
+--     end
+--     print("[MailOrderCatalogs] General: Jeeve's PC detected patching acceptable PC lists")
+
+--     --[[
+--         facing values
+--         0 = north
+--         1 = east
+--         2 = south
+--         3 = west
+--     ]]
+--     local facings = { 2, 3, 0, 1 }
+
+--     for i = 0, 23 do
+--         local sprite = "jpc_tiles_01_" .. i
+--         local facing = facings[(i%4)+1]
+
+--         MailOrderCatalogs_API.registerComputer(sprite, facing)
+--     end
+-- end
+
+-- Events.OnGameStart.Add(patchJeevesPCs)
---
+++ MailOrderCatalogs/42.20/media/lua/shared/MailOrderCatalogs_DeliveryLocations.lua
@@ -0,0 +1,32 @@
+-- MailOrderCatalogs_DeliveryLocations
+local MailOrderCatalogs_DeliveryLocations = {}
+
+local deliveryLocations = {
+    { x = 2046, y = 5912, z = 0 },      -- Brandenberg
+    { x = 684,  y = 9861, z = 0 },      -- Ekron
+    { x = 2446, y = 14532, z = 0 },     -- Irvington
+    { x = 12628, y = 2077, z = 0 },     -- Louisville
+    { x = 10106, y = 12719, z = 0 },    -- March Ridge
+    { x = 6166, y = 5244, z = 0 },      -- Riverside
+    { x = 8130, y = 11288, z = 0 },     -- Rosewood
+    { x = 11970, y = 6918, z = 0 },     -- West Point
+}
+
+local function isDeliveryLocation(x, y, z)
+    for _, loc in ipairs(deliveryLocations) do
+        if loc.x == x and loc.y == y and loc.z == z then
+            return true
+        end
+    end
+    return false
+end
+
+return {
+    deliveryLocations = deliveryLocations,
+    addLocation = function(location)
+        table.insert(deliveryLocations, location)
+    end,
+    isDeliveryLocation = isDeliveryLocation,
+}
+
+-- return MailOrderCatalogs_DeliveryLocations
